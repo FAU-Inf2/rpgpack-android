631b26204339946db5eedbd41bc3665acd7451d1
Julian Wenzel <julian.wenzel@fau.de>
2014-05-21 15:44:59 +0200

    adding main project, try 2

---
+++ src/de/fau/cs/mad/gamekobold/AlljoynInterface.java
@@ -0,0 +1,19 @@
+package de.fau.cs.mad.gamekobold;
+
+import org.alljoyn.bus.BusException;
+import org.alljoyn.bus.annotation.BusInterface;
+import org.alljoyn.bus.annotation.BusMethod;
+
+@BusInterface(name="de.fau.cs.mad.gamekobold.SimpleInterface")
+public interface AlljoynInterface {
+	@BusMethod
+	String Ping(String inStr) throws BusException;
+	
+    @BusMethod
+    String getJSON() throws BusException;
+    
+    @BusMethod
+    String count() throws BusException;
+    
+}
+
---
+++ src/de/fau/cs/mad/gamekobold/MainActivity.java
@@ -0,0 +1,62 @@
+package de.fau.cs.mad.gamekobold;
+
+import android.app.Activity;
+import android.app.ActionBar;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends Activity {
+
+	/* Functions */
+	private void notImplemented() {
+		
+		
+		AlertDialog.Builder builder = new AlertDialog.Builder(this);
+		// Add the buttons
+		builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+		           public void onClick(DialogInterface dialog, int id) {
+		               dialog.cancel();
+		           }
+		       });
+		builder.setMessage("Not Implemented yet!");
+		AlertDialog dialog = builder.create();
+		dialog.show();
+		
+	}
+	
+	public void settings(View view) {
+		this.notImplemented();
+	}
+	
+	public void loadGame(View view) {
+		this.notImplemented();
+	}
+	
+	public void startGame(View view) {
+		this.notImplemented();
+	}
+	public void manageCharackter(View view) {
+		this.notImplemented();
+	}
+	public void templateGenerator(View view) {
+		this.notImplemented();
+	}
+	
+	/* this functions starts an alljoyn test activity */
+	public void alljoyn(View view) {
+    	Intent intent = new Intent(this, AlljoynTest.class);
+    	startActivity(intent);
+	}
+	
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM); 
+        getActionBar().setCustomView(R.layout.main_actionbar);
+        setContentView(R.layout.activity_main);
+    }
+    
+}
