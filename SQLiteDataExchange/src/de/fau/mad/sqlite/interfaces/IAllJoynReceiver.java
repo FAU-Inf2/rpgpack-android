df7c0cbca276e824458c2fe264b4f56e9b78a996
Julian Wenzel <julian.wenzel@fau.de>
2014-05-21 15:26:59 +0200

    removing SQLite-project and appcompat to move it to repository above (not in folder android)

--- SQLiteDataExchange/src/de/fau/mad/sqlite/interfaces/IAllJoynReceiver.java
+++
@@ -1,7 +0,0 @@
-package de.fau.mad.sqlite.interfaces;
-
-import java.util.Map;
-
-public interface IAllJoynReceiver {
-	Map<String, String> receive(String jsonString);
-}
--- SQLiteDataExchange/src/de/fau/mad/sqlite/interfaces/IAllJoynSender.java
+++
@@ -1,5 +0,0 @@
-package de.fau.mad.sqlite.interfaces;
-
-public interface IAllJoynSender {
-	boolean send(String string);
-}
