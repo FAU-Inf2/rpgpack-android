a0383b88a193fb433380a2b4dcb0f9e4982cd7b9
Julian Wenzel <julian.wenzel@fau.de>
2014-07-22 20:56:53 +0200

    smaller modification to 'edit-template' and 'create-new-template' appearance; adapted create-new-matrix-element dialog to resize if keyboard is shown

--- src/de/fau/cs/mad/gamekobold/matrix/MatrixFragment.java
+++ src/de/fau/cs/mad/gamekobold/matrix/MatrixFragment.java
@@ -15,6 +15,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.Button;
@@ -306,6 +307,7 @@ public class MatrixFragment extends GeneralFragment {
 					positiveButton.invalidate();
 				}
 			});
+	        dialog.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_RESIZE);
 			return dialog;
 		}
 	}
--- src/de/fau/cs/mad/gamekobold/template_generator/FolderFragment.java
+++ src/de/fau/cs/mad/gamekobold/template_generator/FolderFragment.java
@@ -21,6 +21,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
