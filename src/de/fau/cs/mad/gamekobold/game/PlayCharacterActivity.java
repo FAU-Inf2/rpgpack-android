51e5ca60e1fb80b3c4e930ffd3f1ccd6687ba65e
Pierre Kiessling <pierre.kiessling@fau.de>
2014-07-28 16:47:17 +0200

    Added actionbar icon to playmode.

--- src/de/fau/cs/mad/gamekobold/game/PlayCharacterActivity.java
+++ src/de/fau/cs/mad/gamekobold/game/PlayCharacterActivity.java
@@ -1,10 +1,20 @@
 package de.fau.cs.mad.gamekobold.game;
 
+import de.fau.cs.mad.gamekobold.R;
 import android.app.Fragment;
+import android.view.Menu;
 
 public class PlayCharacterActivity extends SingleFragmentActivity {
 	@Override
 	protected Fragment createFragment() {
 		return new PlayCharacterFragment();
+	}
+	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.play_actions, menu);
+		return true;
 	}
 }
