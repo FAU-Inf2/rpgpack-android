f173f7041ebac410dea74d6a0e8882464883af8b
Pierre Kiessling <pierre.kiessling@fau.de>
2014-08-05 21:54:59 +0200

    GK-298 #comment implemented simple dragable objects #time 45m

--- src/de/fau/cs/mad/gamekobold/game/ToolboxMapActivity.java
+++ src/de/fau/cs/mad/gamekobold/game/ToolboxMapActivity.java
@@ -3,11 +3,17 @@ package de.fau.cs.mad.gamekobold.game;
 import de.fau.cs.mad.gamekobold.R;
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.DragEvent;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.View.DragShadowBuilder;
+import android.view.View.OnDragListener;
+import android.view.View.OnTouchListener;
+import android.view.ViewGroup;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
 
-public class ToolboxMapActivity extends Activity {
+public class ToolboxMapActivity extends Activity implements OnTouchListener, OnDragListener{
 
 	private ImageButton currPaint;
 	private ToolboxMapView mapView;
@@ -23,6 +29,34 @@ public class ToolboxMapActivity extends Activity {
 		currPaint = (ImageButton) paintLayout.getChildAt(0);
 		currPaint.setImageDrawable(getResources().getDrawable(
 				R.drawable.paint_pressed));
+		findViewById(R.id.red_dot).setOnTouchListener(this);
+		findViewById(R.id.map).setOnDragListener(this);
+		findViewById(R.id.paint_colors).setOnDragListener(this);
+	}
+	
+	@Override
+	public boolean onTouch(View v, MotionEvent e) {
+		if (e.getAction() == MotionEvent.ACTION_DOWN) {
+			DragShadowBuilder shadowBuilder = new View.DragShadowBuilder(v);
+		    	v.startDrag(null, shadowBuilder, v, 0);
+		    v.setVisibility(View.INVISIBLE);
+		    return true;
+		} else {
+		    return false;
+		}
+	}
+	
+	@Override
+	public boolean onDrag(View v, DragEvent e) {
+		if (e.getAction()==DragEvent.ACTION_DROP) {
+			View view = (View) e.getLocalState();
+			ViewGroup from = (ViewGroup) view.getParent();
+			from.removeView(view);
+			LinearLayout to = (LinearLayout) v;
+			to.addView(view);
+			view.setVisibility(View.VISIBLE);
+		}
+		return true;
 	}
 
 	public void iconClicked(View view) {
