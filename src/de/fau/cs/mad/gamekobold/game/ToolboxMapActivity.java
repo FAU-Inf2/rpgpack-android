82e81b4530ded0d9f207583e3a39a95b547c62ed
Pierre Kiessling <pierre.kiessling@fau.de>
2014-08-05 20:33:29 +0200

    GK-298 #comment more intuitive color picking #time 33m

--- src/de/fau/cs/mad/gamekobold/game/ToolboxMapActivity.java
+++ src/de/fau/cs/mad/gamekobold/game/ToolboxMapActivity.java
@@ -1,39 +1,47 @@
 package de.fau.cs.mad.gamekobold.game;
 
-
 import de.fau.cs.mad.gamekobold.R;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
-import de.fau.cs.mad.gamekobold.R;
 
-public class ToolboxMapActivity extends Activity{
-	
+public class ToolboxMapActivity extends Activity {
+
 	private ImageButton currPaint;
 	private ToolboxMapView mapView;
-	
-	
+	private LinearLayout paintLayout;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
-		//hier fehlt evtl noch was
-			mapView = (ToolboxMapView)findViewById(R.id.map);
-			LinearLayout paintLayout = (LinearLayout)findViewById(R.id.paint_colors);
-			currPaint = (ImageButton)paintLayout.getChildAt(0);
-			currPaint.setImageDrawable(getResources().getDrawable(R.drawable.ic_launcher));
+
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_game_toolbox_map);
+		mapView = (ToolboxMapView) findViewById(R.id.map);
+		paintLayout = (LinearLayout) findViewById(R.id.paint_colors);
+		currPaint = (ImageButton) paintLayout.getChildAt(0);
+		currPaint.setImageDrawable(getResources().getDrawable(
+				R.drawable.paint_pressed));
 	}
-	
-	public void paintClicked(View view){
-		if(view!=currPaint){
-			ImageButton imgView = (ImageButton)view;
+
+	public void iconClicked(View view) {
+		if (view != currPaint) {
+			ImageButton imgView = (ImageButton) view;
 			String color = view.getTag().toString();
 			mapView.setColor(color);
-			
-			imgView.setImageDrawable(getResources().getDrawable(R.drawable.paint_pressed));
-			currPaint.setImageDrawable(getResources().getDrawable(R.drawable.ic_launcher));
-			currPaint=(ImageButton)view;
+			int size = paintLayout.getChildCount();
+			for (int i = 0; i < size; i++) {
+				if (paintLayout.getChildAt(i) instanceof ImageButton) {
+					ImageButton tmp = (ImageButton) paintLayout.getChildAt(i);
+					tmp.setImageDrawable(getResources().getDrawable(
+							R.drawable.paint_unpressed));
+				}
 			}
+			imgView.setImageDrawable(getResources().getDrawable(
+					R.drawable.paint_pressed));
+			currPaint = (ImageButton) view;
+		}
 	}
 
 }
