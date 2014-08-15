41b72543a6f2b2b2aa4963d7e7c695ca767771b6
Benjamin Röder <benjamin@roederclan.de>
2014-08-15 17:29:14 +0200

    TemplateLab now loading user generated templates and characters

--- src/de/fau/cs/mad/gamekobold/game/TemplateLab.java
+++ src/de/fau/cs/mad/gamekobold/game/TemplateLab.java
@@ -1,20 +1,138 @@
 package de.fau.cs.mad.gamekobold.game;
 
+import java.io.File;
 import java.util.ArrayList;
 
+import de.fau.cs.mad.gamekobold.jackson.CharacterSheet;
+import de.fau.cs.mad.gamekobold.jackson.JacksonInterface;
 import de.fau.cs.mad.gamekobold.templatebrowser.Template;
 import android.content.Context;
 import android.util.Log;
 
 public class TemplateLab {
+	// tag for logging
+	private static String LOG_TAG = "TemplateLab";
+	
 	private static TemplateLab sTemplateLab;
 	private ArrayList<Template> templates;
 	private Context appContext;
+	// time stamp for the templates folder
+	private long folderTimeStamp;
 
 	TemplateLab(Context appContext) {
 		this.appContext = appContext;
 		templates = new ArrayList<Template>();
+		// set time stamp to 0, so we are at least loading once
+		folderTimeStamp = 0;
+	}
 
+	public static TemplateLab get(Context c) {
+		if (sTemplateLab == null) {
+			sTemplateLab = new TemplateLab(c.getApplicationContext());
+		}
+		return sTemplateLab;
+	}
+
+	public ArrayList<Template> getTemplates() {
+		// checks if a template has been created or deleted
+		if(!checkForTemplateDirectoryChange()) {
+			// only check every template for a change when not reloading the whole list
+		}
+		return templates;
+	}
+
+	public Template getGame(String templateName) {
+		for (Template t : templates) {
+			if (t.getTemplateName().equals(templateName))
+				return t;
+		}
+		return null;
+	}
+
+	/**
+	 * Checks the template directory for a change. Its time stamp is updated
+	 * when a file is created or deleted.
+	 * @return true if the template list has been reloaded, false otherwise.
+	 */
+	private boolean checkForTemplateDirectoryChange() {
+		final File templateDir = JacksonInterface.getTemplateRootDirectory(appContext);
+		if(templateDir != null) {
+			final long newTimeStamp = templateDir.lastModified();
+			if(folderTimeStamp < newTimeStamp) {
+				templates.clear();
+				// add default test data
+				addDefaultData();
+				// update time stamp
+				folderTimeStamp = newTimeStamp;
+				// reload template list
+				Log.d(LOG_TAG, "Reloading template list");
+				Log.d(LOG_TAG, "Tempolate directory:" + templateDir.getAbsolutePath());
+				if (templateDir.isDirectory()) {
+					final File[] fileList = templateDir.listFiles();
+					de.fau.cs.mad.gamekobold.jackson.Template loadedTemplate = null;
+					for (final File file : fileList) {
+						try {
+							loadedTemplate = JacksonInterface.loadTemplate(file, true); 
+							if (loadedTemplate != null) {
+								Template temp = new Template(
+										loadedTemplate.templateName,
+										loadedTemplate.gameName,
+										loadedTemplate.author, loadedTemplate.date,
+										loadedTemplate.iconID,
+										loadedTemplate.description);
+								if (temp.getTemplateName().equals("")) {
+									temp.setTemplateName(file.getName());
+								}
+								temp.fileAbsolutePath = file.getAbsolutePath();
+								// set time stamp
+								temp.setFileTimeStamp(file.lastModified());
+								//load the characters for the template
+								loadCharacters(temp);
+								// add the template to the list
+								templates.add(temp);
+							}
+						} catch (Throwable e) {
+							e.printStackTrace();
+						}
+					}
+				}
+				// fake item to create New Character from template
+				GameCharacter createNewCharacter = new GameCharacter("+");
+				for (Template t : templates) {
+					t.addCharacter(createNewCharacter);
+				}
+				return true;
+			}
+		}
+		return false;
+	}
+
+	/**
+	 * Loads and adds the characters for the given template.
+	 * @param template
+	 */
+	private void loadCharacters(Template template) {
+		final File characterDir = JacksonInterface.getDirectoryForCharacters(template, appContext, false);
+		if(characterDir != null) {
+			final File[] characters = characterDir.listFiles();
+			for(final File characterFile : characters) {
+				try {
+					final CharacterSheet sheet = JacksonInterface.loadCharacterSheet(characterFile, true);
+					final GameCharacter character = new GameCharacter("");
+					character.setCharacterName(sheet.name);
+					character.setDescription(sheet.description);
+					character.setTemplate(template);
+					character.setFileAbsPath(characterFile.getAbsolutePath());
+					template.addCharacter(character);
+				}
+				catch(Throwable e) {
+					e.printStackTrace();
+				}
+			}
+		}
+	}
+
+	private void addDefaultData() {
 		Template template1 = new Template("My First Template",
 				"Dungeons and Dragons", "Anna", "20.05.2014");
 		Template template2 = new Template("The Best Template",
@@ -54,31 +172,5 @@ public class TemplateLab {
 		templates.add(template2);
 		templates.add(template3);
 		templates.add(template4);
-
-		// fake item to create New Character from template
-		GameCharacter createNewCharacter = new GameCharacter("+");
-		for (Template t : templates) {
-			t.addCharacter(createNewCharacter);
-		}
-
-	}
-
-	public static TemplateLab get(Context c) {
-		if (sTemplateLab == null) {
-			sTemplateLab = new TemplateLab(c.getApplicationContext());
-		}
-		return sTemplateLab;
-	}
-
-	public ArrayList<Template> getTemplates() {
-		return templates;
-	}
-
-	public Template getGame(String templateName) {
-		for (Template t : templates) {
-			if (t.getTemplateName().equals(templateName))
-				return t;
-		}
-		return null;
 	}
 }
