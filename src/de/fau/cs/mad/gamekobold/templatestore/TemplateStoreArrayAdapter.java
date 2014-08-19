da6b46f2883eb150c4841b2e33d20345dd2a377f
Gregor Völkl <gregor.voelkl@fau.de>
2014-08-19 21:44:27 +0200

    added rating bar to template list view

--- src/de/fau/cs/mad/gamekobold/templatestore/TemplateStoreArrayAdapter.java
+++ src/de/fau/cs/mad/gamekobold/templatestore/TemplateStoreArrayAdapter.java
@@ -8,6 +8,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.RatingBar;
 import android.widget.TextView;
 
 public class TemplateStoreArrayAdapter extends ArrayAdapter<StoreTemplate> {
@@ -38,6 +39,9 @@ public class TemplateStoreArrayAdapter extends ArrayAdapter<StoreTemplate> {
 			
 			TextView name = (TextView) rowView.findViewById(R.id.tv_store_name);
 			name.setText(curr.getName());
+			
+			RatingBar bar = (RatingBar) rowView.findViewById(R.id.ratingBarStore);
+			bar.setRating(curr.getRating());
 			
 		} else {
 			return convertView;		
