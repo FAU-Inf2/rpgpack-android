c51071451d2bb9e46177736cc7660e826ab1d361
Pierre Kiessling <pierre.kiessling@fau.de>
2014-08-23 09:34:51 +0200

    GK-298 #comment added skeleton for drag and drop tests and copied actionbar menu to gamebrowser because characterview is broken #time 50m

---
+++ src/de/fau/cs/mad/gamekobold/game/ToolboxTestActivity.java
@@ -0,0 +1,16 @@
+package de.fau.cs.mad.gamekobold.game;
+
+import de.fau.cs.mad.gamekobold.R;
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ToolboxTestActivity extends Activity{
+
+	
+		@Override
+		protected void onCreate(Bundle savedInstanceState) {
+
+			super.onCreate(savedInstanceState);
+			setContentView(R.layout.activity_game_toolbox_test);
+		}
+}
