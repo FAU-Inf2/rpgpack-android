e19e08ab6b6603bb96cddd14d0b9b44e92bf9b80
Gregor Völkl <gregor.voelkl@fau.de>
2014-09-01 21:14:52 +0200

    fixed issue in alternating row color when scrolling fast or reloading

--- src/de/fau/cs/mad/gamekobold/templatestore/TemplateStoreArrayAdapter.java
+++ src/de/fau/cs/mad/gamekobold/templatestore/TemplateStoreArrayAdapter.java
@@ -14,7 +14,7 @@ import android.widget.RatingBar;
 import android.widget.TextView;
 
 public class TemplateStoreArrayAdapter extends ArrayAdapter<StoreTemplate> {
-	
+
 	static class ViewHolder {
 		TextView worldname;
 		TextView date_author;
@@ -23,61 +23,74 @@ public class TemplateStoreArrayAdapter extends ArrayAdapter<StoreTemplate> {
 		ImageView img;
 		Bitmap bm;
 	}
-	
+
 	Context context;
 	List<StoreTemplate> templates;
-	
-	public TemplateStoreArrayAdapter(Context context, List<StoreTemplate> templates) {
+
+	public TemplateStoreArrayAdapter(Context context,
+			List<StoreTemplate> templates) {
 		super(context, R.layout.template_store_rowlayout, templates);
 		this.templates = templates;
 		this.context = context;
 	}
-	
-	public View getView(int position, View convertView, ViewGroup parent)  {
-		LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+	public View getView(int position, View convertView, ViewGroup parent) {
+		LayoutInflater inflater = (LayoutInflater) context
+				.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 
 		ViewHolder holder;
-		
+
 		StoreTemplate curr = templates.get(position);
-		
-		if(convertView == null) {
-			convertView = inflater.inflate(R.layout.template_store_rowlayout, parent, false);
+
+		if (convertView == null) {
+			convertView = inflater.inflate(R.layout.template_store_rowlayout,
+					parent, false);
 			holder = new ViewHolder();
-			holder.worldname = (TextView) convertView.findViewById(R.id.tv_store_worldname);
-			holder.date_author = (TextView) convertView.findViewById(R.id.tv_store_date_author);
-			holder.name = (TextView) convertView.findViewById(R.id.tv_store_name);
-			holder.bar = (RatingBar) convertView.findViewById(R.id.ratingBarStore);
-			holder.img = (ImageView) convertView.findViewById(R.id.templateStoreImg);
-			if(curr.hasImage()) {
+			holder.worldname = (TextView) convertView
+					.findViewById(R.id.tv_store_worldname);
+			holder.date_author = (TextView) convertView
+					.findViewById(R.id.tv_store_date_author);
+			holder.name = (TextView) convertView
+					.findViewById(R.id.tv_store_name);
+			holder.bar = (RatingBar) convertView
+					.findViewById(R.id.ratingBarStore);
+			holder.img = (ImageView) convertView
+					.findViewById(R.id.templateStoreImg);
+			if (curr.hasImage()) {
 				curr.setBm(curr.getImage_data());
 			}
 			convertView.setTag(holder);
 		} else {
 			holder = (ViewHolder) convertView.getTag();
 		}
-			
-			
-			holder.worldname.setText(curr.getWorldname());
-			holder.date_author.setText(curr.getDate()+" - " + curr.getAuthor());
-			holder.name.setText(curr.getName());
-			if(holder.bar != null) {
-				holder.bar.setRating(curr.getRating());
-			}
-			
-			if((position % 2) == 0) {
-				convertView.setBackgroundColor( context.getResources().getColor(R.color.background_green) );
-			}
 
-			if(curr.hasImage()) {
-				//byte[] decodedString = Base64.decode(curr.getImage_data(), Base64.DEFAULT);
-				//Bitmap decodedByte = BitmapFactory.decodeByteArray(decodedString, 0, decodedString.length); 
-				if(curr.getBm() == null) {
-					curr.setBm(curr.getImage_data());
-				}
-				holder.img.setImageBitmap(curr.getBm());
+		holder.worldname.setText(curr.getWorldname());
+		holder.date_author.setText(curr.getDate() + " - " + curr.getAuthor());
+		holder.name.setText(curr.getName());
+		if (holder.bar != null) {
+			holder.bar.setRating(curr.getRating());
+		}
+
+		if ((position % 2) == 0) {
+			convertView.setBackgroundColor(context.getResources().getColor(
+					R.color.background_green));
+		} else {
+			convertView.setBackgroundColor(context.getResources().getColor(
+					R.color.background_dark));			
+		}
+
+		if (curr.hasImage()) {
+			// byte[] decodedString = Base64.decode(curr.getImage_data(),
+			// Base64.DEFAULT);
+			// Bitmap decodedByte = BitmapFactory.decodeByteArray(decodedString,
+			// 0, decodedString.length);
+			if (curr.getBm() == null) {
+				curr.setBm(curr.getImage_data());
 			}
-		
-			return convertView;	
+			holder.img.setImageBitmap(curr.getBm());
+		}
+
+		return convertView;
 	}
 
 	public void add(StoreTemplate tpl) {
