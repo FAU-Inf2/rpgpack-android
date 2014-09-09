40a220e537f3c33610c7be1ca0708545f3c9e0f0
Pierre Kiessling <pierre.kiessling@fau.de>
2014-09-09 12:39:46 +0200

    GK-298 #comment made gridview relative to displaysize and fixed dotline not showing up (time includes google search) #time 3h 30m

--- src/de/fau/cs/mad/gamekobold/game/ToolboxTestActivity.java
+++ src/de/fau/cs/mad/gamekobold/game/ToolboxTestActivity.java
@@ -7,8 +7,10 @@ import android.app.Activity;
 import android.content.Context;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
+import android.util.Log;
 import android.view.Display;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.WindowManager;
 import android.widget.AdapterView;
 import android.widget.GridLayout;
@@ -21,53 +23,65 @@ import android.widget.AdapterView.OnItemClickListener;
 public class ToolboxTestActivity extends Activity {
 
 	Activity mContext;
-	int mWidth;
-	int mHeight;
+	float mWidth;
+	float mHeight;
 	private String[] dots = { "red", "green", "blue", "black", "orange" };
 	private ArrayList<String> testCells = new ArrayList<String>();
 	private int mNumCells;
 	private int mNumLines;
 	private int mNumColumns;
-	private int cellWidth = 30;
-	private int dot_size = 25;
+	private int cell_size;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 
-		setContentView(R.layout.activity_game_toolbox_test);
 		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_game_toolbox_test);
 		initTest();
 		createDots();
 		createCells();
-		setContentView(R.layout.activity_game_toolbox_test);
+		// setContentView(R.layout.activity_game_toolbox_test);
 	}
 
 	protected void initTest() {
 		DisplayMetrics displaymetrics = new DisplayMetrics();
-		WindowManager wm = (WindowManager) getApplicationContext().getSystemService(Context.WINDOW_SERVICE);
+		WindowManager wm = (WindowManager) getApplicationContext()
+				.getSystemService(Context.WINDOW_SERVICE);
 		wm.getDefaultDisplay().getMetrics(displaymetrics);
-		mWidth = displaymetrics.widthPixels;
-		mHeight = displaymetrics.heightPixels - 20;
-		mNumColumns = mWidth / cellWidth;
-		mNumLines = mHeight / cellWidth;
+		float density = getResources().getDisplayMetrics().density;
+		cell_size = (int) ((float) getResources().getDimension(
+				R.dimen.game_toolbox_map_cellsize) / density);
+		mWidth = displaymetrics.widthPixels / density;
+		mHeight = displaymetrics.heightPixels / density - (90 + cell_size);
+		mNumColumns = (int) (mWidth / cell_size);
+		mNumLines = (int) (mHeight / cell_size);
 		mNumCells = mNumLines * mNumColumns;
+		Log.i("Width", "" + mWidth);
+		Log.i("Cell_Size", "" + cell_size);
+		Log.i("Height", "" + mHeight);
+		Log.i("Cells", "" + mNumCells);
+		Log.i("mNumColumns", "" + mNumColumns);
+		Log.i("mNumLines", "" + mNumLines);
 	}
 
 	public void createCells() {
-		
+
 		for (int i = 0; i < mNumCells; i++) {
 			testCells.add(String.valueOf(i));
 		}
 
-		RelativeLayout relativeLayout = (RelativeLayout) findViewById(R.id.test_layout);
+		LinearLayout linearLayout = (LinearLayout) findViewById(R.id.test_layout);
 		GridView grid = (GridView) new GridView(ToolboxTestActivity.this);
+		// ViewGroup.LayoutParams lp=new
+		// ViewGroup.LayoutParams(cellWidth,cellWidth);
+		// grid.setLayoutParams(lp);
 
 		ToolboxTestGridElementAdapter adapter = new ToolboxTestGridElementAdapter(
 				ToolboxTestActivity.this, testCells);
 		grid.setNumColumns(mNumColumns);
 		grid.setAdapter(adapter);
-	
-		relativeLayout.addView(grid);
+
+		linearLayout.addView(grid);
 
 	}
 
@@ -77,7 +91,7 @@ public class ToolboxTestActivity extends Activity {
 		for (String item : dots) {
 			ImageView img_view = (ImageView) new ImageView(this);
 			LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
-					dot_size, dot_size);
+					cell_size, cell_size);
 
 			if (item.equals("red")) {
 				img_view.setTag(item);
