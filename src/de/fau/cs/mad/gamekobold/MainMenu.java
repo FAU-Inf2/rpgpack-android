5036759dbb7594f7627eb10594048bba9fd0bf1d
Benjamin Röder <benjamin@roederclan.de>
2014-09-21 18:15:38 +0200

    improved the layout for the new menu. removed menu_gradient_shape because it is not needed. added double back press feature and intents for starting activities

--- src/de/fau/cs/mad/gamekobold/MainMenu.java
+++ src/de/fau/cs/mad/gamekobold/MainMenu.java
@@ -1,9 +1,14 @@
 package de.fau.cs.mad.gamekobold;
 
+import de.fau.cs.mad.gamekobold.characterbrowser.CharacterBrowserActivity;
+import de.fau.cs.mad.gamekobold.game.GameBrowserActivity;
+import de.fau.cs.mad.gamekobold.templatebrowser.TemplateBrowserActivity;
 import android.annotation.SuppressLint;
 import android.app.ActionBar;
 import android.app.Activity;
+import android.content.Intent;
 import android.graphics.Color;
+import android.graphics.Paint;
 import android.graphics.drawable.GradientDrawable;
 import android.graphics.drawable.GradientDrawable.Orientation;
 import android.os.Bundle;
@@ -12,29 +17,43 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainMenu extends Activity implements OnClickListener{
 	private LinearLayout row1 = null, row2 = null, row3 = null;
 
+	// toast for managing double press to exit feature
+	private Toast leaveToast = null;
+	
+	@SuppressLint("ShowToast")
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		getActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
 		getActionBar().setCustomView(R.layout.main_actionbar);
 		setContentView(R.layout.menu_with_3_buttons);
+		
+		// create toast for managing double press to exit feature
+		leaveToast = Toast.makeText(this, getString(R.string.leave_toast),Toast.LENGTH_SHORT);
 
+		// get the three rows
 		row1 = (LinearLayout)findViewById(R.id.row1);
 		row2 = (LinearLayout)findViewById(R.id.row2);
 		row3 = (LinearLayout)findViewById(R.id.row3);
 
+		// set their on click listener
 		row1.setOnClickListener(this);
 		row2.setOnClickListener(this);
 		row3.setOnClickListener(this);
 
-		ImageView row1IV = (ImageView) row1.findViewById(R.id.ImageView02);
-		ImageView row2IV = (ImageView) row2.findViewById(R.id.ImageView01);
-		ImageView row3IV = (ImageView) row3.findViewById(R.id.imageView1);
+		// get the three image views
+		ImageView row1IV = (ImageView) row1.findViewById(R.id.row1_imageView);
+		ImageView row2IV = (ImageView) row2.findViewById(R.id.row2_imageView);
+		ImageView row3IV = (ImageView) row3.findViewById(R.id.row3_imageView);
 
+		// create the 3 gradient drawables with standard colors of black and white and add them to
+		// the image views
 		int[] standardColors = {
 			Color.BLACK,
 			Color.WHITE
@@ -45,29 +64,59 @@ public class MainMenu extends Activity implements OnClickListener{
 		row2IV.setImageDrawable(row2Gradient);
 		GradientDrawable row3Gradient = new GradientDrawable(Orientation.TOP_BOTTOM, standardColors);
 		row3IV.setImageDrawable(row3Gradient);
-		// Edit your colors here
-		// button 1
+
+		// get all the text views
+		TextView row1Main = (TextView) row1.findViewById(R.id.row1_textView_main);
+		TextView row1Description = (TextView) row1.findViewById(R.id.row1_textView_description);
+		TextView row2Main = (TextView) row2.findViewById(R.id.row2_textView_main);
+		TextView row2Description = (TextView) row2.findViewById(R.id.row2_textView_description);
+		TextView row3Main = (TextView) row3.findViewById(R.id.row3_textView_main);
+		TextView row3Description = (TextView) row3.findViewById(R.id.row3_textView_description);
+
+		// underline the main texts
+		row1Main.setPaintFlags(row1Main.getPaintFlags() | Paint.UNDERLINE_TEXT_FLAG);
+		row2Main.setPaintFlags(row1Main.getPaintFlags() | Paint.UNDERLINE_TEXT_FLAG);
+		row3Main.setPaintFlags(row1Main.getPaintFlags() | Paint.UNDERLINE_TEXT_FLAG);
+		
+		// START EDITING HERE
+		
+		// gradient colors
+		// button 1 gradient color
 		int[] gradient1 = {
 			// start color
 			Color.rgb(255, 0, 0),
 			// end color
 			Color.rgb(100, 0, 0)
 		};
-		// button 2
+		// button 2 gradient color
 		int[] gradient2 = {
 			// start color
 			Color.rgb(0, 255, 0),
 			// end color
 			Color.rgb(0, 100, 0)
 		};
-		// button 3
+		// button 3 gradient color
 		int[] gradient3 = {
 			// start color
 			Color.rgb(0, 0, 255),
 			// end color
 			Color.rgb(0, 0, 100)
 		};
-		// Stop editing here
+
+		// Set the texts
+		// button 1
+		row1Main.setText("Game Browser");
+		row1Description.setText("foo");
+		// button 2
+		row2Main.setText("Character Browser");
+		row2Description.setText("foo");
+		// button 3
+		row3Main.setText("Template Browser");
+		row3Description.setText("foo");
+		
+		// STOP EDITING HERE
+		
+		// set the colors for the gradients
 		setColors(gradient1, row1Gradient);
 		setColors(gradient2, row2Gradient);
 		setColors(gradient3, row3Gradient);
@@ -81,6 +130,7 @@ public class MainMenu extends Activity implements OnClickListener{
 	 */
 	@SuppressLint("NewApi")
 	private void setColors(int [] colors, GradientDrawable drawable) {
+		// check sdl level
 		if(android.os.Build.VERSION.SDK_INT >= 16) {
 			drawable.setColors(colors);			
 		}
@@ -94,14 +144,28 @@ public class MainMenu extends Activity implements OnClickListener{
 		if(v == row1) {
 			// button 1 action
 			Log.d("MainMenu", "clicked row1");
+			Intent intent = new Intent(MainMenu.this, GameBrowserActivity.class);
+			startActivity(intent);
 		}
 		else if(v == row2) {
 			// button 2 action
 			Log.d("MainMenu", "clicked row2");
+			Intent intent = new Intent(MainMenu.this, CharacterBrowserActivity.class);
+			startActivity(intent);
 		}
 		else if(v == row3) {
 			// button 3 action
-			Log.d("MainMenu", "clicked row3");			
+			Log.d("MainMenu", "clicked row3");
+			Intent intent = new Intent(MainMenu.this, TemplateBrowserActivity.class);
+			startActivity(intent);
+		}
+	}
+
+	public void onBackPressed() {
+		if (!leaveToast.getView().isShown()) {
+			leaveToast.show();
+		} else {
+			super.onBackPressed();
 		}
 	}
 }
