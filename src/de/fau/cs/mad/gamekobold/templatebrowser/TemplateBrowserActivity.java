70f3175956f2215f67a38493dda0d44c4906375f
Benjamin Röder <benjamin@roederclan.de>
2014-09-25 14:56:36 +0200

    removed template file import from template browser

--- src/de/fau/cs/mad/gamekobold/templatebrowser/TemplateBrowserActivity.java
+++ src/de/fau/cs/mad/gamekobold/templatebrowser/TemplateBrowserActivity.java
@@ -34,7 +34,7 @@ import de.fau.cs.mad.gamekobold.jackson.JacksonFileValidator;
 import de.fau.cs.mad.gamekobold.jackson.JacksonInterface;
 import de.fau.cs.mad.gamekobold.template_generator.TemplateGeneratorActivity;
 
-public class TemplateBrowserActivity extends ListActivity implements IFileBrowserReceiver {
+public class TemplateBrowserActivity extends ListActivity {
 	private List<Template> templateList = null;
 	// time stamp of the template directory
 	// with this we can determine if a template has been deleted / created
@@ -254,11 +254,7 @@ public class TemplateBrowserActivity extends ListActivity implements IFileBrowse
 		int id = item.getItemId();
 		if (id == R.id.action_settings) {
 			return true;
-		}
-		else if(id == R.id.menu_item_import_template) {
-			showFileBrowserPopup();
-			return true;
-		}
+		}		
 		return super.onOptionsItemSelected(item);
 	}
 
@@ -484,51 +480,6 @@ public class TemplateBrowserActivity extends ListActivity implements IFileBrowse
 			adapter.clear();
 			adapter.addAll(templateList);
 			adapter.notifyDataSetChanged();
-		}
-	}
-
-	/**
-	 * Shows the FileBrowser as a popup.
-	 */
-	private void showFileBrowserPopup() {
-		FileBrowser.showAsPopup(getFragmentManager(), FileBrowser.newInstance(this, FileBrowser.Mode.PICK_FILE));
-		Toast.makeText(this, getString(R.string.toast_fileexplorer_msg_pick_template), Toast.LENGTH_LONG).show();
-	}
-
-	/**
-	 * Callback for FileBrowser. This is called when the user picked a file to import.
-	 */
-	@Override
-	public void onFilePicked(File file) {
-		// close file browser popup
-		FileBrowser.removeAsPopup(getFragmentManager());
-		// check if selected file is a template
-		if(JacksonFileValidator.isValidTemplate(file)) {
-			// get template directory
-			File templateRootDir = JacksonInterface.getTemplateRootDirectory(this);
-			try {
-				// try to copy file to template directory
-				FileCopyUtility.copyFile(file, new File(templateRootDir, file.getName()), false);
-				// reload template list
-				loadTemplateList();
-				// inform user
-				Toast.makeText(this, getString(R.string.toast_imported_template), Toast.LENGTH_LONG).show();
-			}
-			catch(IOException e) {
-				e.printStackTrace();
-				// failed to import template, inform user
-				Toast.makeText(this, getString(R.string.toast_imported_template_failed), Toast.LENGTH_LONG).show();
-			} catch (FileWouldOverwriteException e) {
-				// we would overwrite an existing file
-				e.printStackTrace();
-				// TODO show popup and let user decide
-				// failed to import template, inform user
-				Toast.makeText(this, getString(R.string.toast_imported_template_failed), Toast.LENGTH_LONG).show();
-			}
-		}
-		else {
-			// file is not a template, inform user
-			Toast.makeText(this, getString(R.string.toast_file_is_not_template), Toast.LENGTH_LONG).show();			
 		}
 	}
 }
