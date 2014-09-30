8d452a70b3159ae8c136540c3ab9c4863fb420a7
Anna Tertilt <Anna.Tertilt@studium.fau.de>
2014-09-30 15:07:59 +0200

    For consistency added an extra deletion of deleted characters in games too. User becomes a warning, that the character to delete will be deleted from all games too.

--- src/de/fau/cs/mad/gamekobold/characterbrowser/CharacterBrowserActivity.java
+++ src/de/fau/cs/mad/gamekobold/characterbrowser/CharacterBrowserActivity.java
@@ -6,6 +6,9 @@ import java.util.Collections;
 import java.util.List;
 
 import de.fau.cs.mad.gamekobold.R;
+import de.fau.cs.mad.gamekobold.game.Game;
+import de.fau.cs.mad.gamekobold.game.GameCharacter;
+import de.fau.cs.mad.gamekobold.game.GameLab;
 import de.fau.cs.mad.gamekobold.jackson.CharacterSheet;
 import de.fau.cs.mad.gamekobold.jackson.JacksonInterface;
 import de.fau.cs.mad.gamekobold.templatebrowser.CharacterDetailsActivity;
@@ -14,6 +17,7 @@ import android.app.ListActivity;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
@@ -30,7 +34,8 @@ public class CharacterBrowserActivity extends ListActivity {
 		setListAdapter(adapter);
 		getListView().setOnItemClickListener(new OnItemClickListener() {
 			@Override
-			public void onItemClick(AdapterView<?> arg0, View view, int position, long id) {
+			public void onItemClick(AdapterView<?> arg0, View view,
+					int position, long id) {
 				// edit character
 				Intent i = new Intent(CharacterBrowserActivity.this,
 						CharacterDetailsActivity.class);
@@ -39,52 +44,87 @@ public class CharacterBrowserActivity extends ListActivity {
 				startActivity(i);
 			}
 		});
-		
+
 		getListView().setOnItemLongClickListener(
-			new AdapterView.OnItemLongClickListener() {
-				@Override
-				public boolean onItemLongClick(AdapterView<?> arg0, View view, int position, long id) {
-					if(position < adapter.getCount()-1) {
-						final CharacterSheet clickedCharacter = adapter.getItem(position);
-						AlertDialog.Builder builder = new AlertDialog.Builder(
-								CharacterBrowserActivity.this);
-						builder.setTitle(getResources().getString(
-								R.string.msg_ask_character_deletion));
-						builder.setMessage(getResources().getString(
-								R.string.msg_yes_to_delete_character));
-						builder.setNegativeButton(
-								getResources().getString(R.string.no),
-								new DialogInterface.OnClickListener() {
-									@Override
-									public void onClick( DialogInterface dialog, int which) {
-									}
-								});
-						builder.setPositiveButton(
-								getResources().getString(R.string.yes),
-								new DialogInterface.OnClickListener() {
-									@Override
-									public void onClick(
-											DialogInterface dialog,
-											int which) {
-										File file = new File(clickedCharacter.getFileAbsolutePath());
-										if (file != null) {
-											// delete character
-											if (file.isFile()) {
-												file.delete();
-												adapter.remove(clickedCharacter);
-												adapter.notifyDataSetChanged();
-											}
+				new AdapterView.OnItemLongClickListener() {
+					@Override
+					public boolean onItemLongClick(AdapterView<?> arg0,
+							View view, int position, long id) {
+						if (position < adapter.getCount() - 1) {
+							final CharacterSheet clickedCharacter = adapter
+									.getItem(position);
+							AlertDialog.Builder builder = new AlertDialog.Builder(
+									CharacterBrowserActivity.this);
+							builder.setTitle(getResources().getString(
+									R.string.msg_ask_character_deletion));
+							builder.setMessage(getResources().getString(
+									R.string.msg_yes_to_delete_character));
+							builder.setNegativeButton(
+									getResources().getString(R.string.no),
+									new DialogInterface.OnClickListener() {
+										@Override
+										public void onClick(
+												DialogInterface dialog,
+												int which) {
 										}
-									}
-								});
-						builder.create().show();
-						return true;
+									});
+							builder.setPositiveButton(
+									getResources().getString(R.string.yes),
+									new DialogInterface.OnClickListener() {
+										@Override
+										public void onClick(
+												DialogInterface dialog,
+												int which) {
+											// TODO remove character
+											// from all games too
+											ArrayList<Game> games = new ArrayList<Game>();
+											games.addAll(GameLab
+													.get(CharacterBrowserActivity.this)
+													.getGames());
+
+											ArrayList<GameCharacter> characters = new ArrayList<GameCharacter>();
+											// check if character to delete
+											// appears in games
+											for (Game g : games) {
+												characters.addAll(g
+														.getCharacterList());
+												int location = 0;
+												for (GameCharacter c : characters) {
+													if (c.getFileAbsPath()
+															.equals(clickedCharacter
+																	.getFileAbsolutePath())) {
+														g.getCharacterList()
+																.remove(location);
+														Log.d("CharacterBrowserActivity",
+																c.getCharacterName()
+																		+ " was deleted!!!!");
+													}
+													location++;
+												}
+												characters.clear();
+											}
+											File file = new File(
+													clickedCharacter
+															.getFileAbsolutePath());
+											if (file != null) {
+												// delete character
+												if (file.isFile()) {
+													file.delete();
+													adapter.remove(clickedCharacter);
+													adapter.notifyDataSetChanged();
+												}
+											}
+
+										}
+									});
+							builder.create().show();
+							return true;
+						}
+						return false;
 					}
-					return false;
-				}
-		});
+				});
 	}
-	
+
 	@Override
 	protected void onResume() {
 		super.onResume();
@@ -93,31 +133,32 @@ public class CharacterBrowserActivity extends ListActivity {
 		Collections.sort(adapter.getList());
 		adapter.notifyDataSetChanged();
 	}
-	
+
 	private void loadCharacterList() {
-		final File rootCharacterDir = JacksonInterface.getCharacterRootDirectory(this);
-		if(rootCharacterDir == null) {
+		final File rootCharacterDir = JacksonInterface
+				.getCharacterRootDirectory(this);
+		if (rootCharacterDir == null) {
 			return;
 		}
-		if(!rootCharacterDir.isDirectory()) {
+		if (!rootCharacterDir.isDirectory()) {
 			return;
 		}
 		final File[] characterFolders = rootCharacterDir.listFiles();
 		CharacterSheet charSheet = null;
-		for(final File charFolder : characterFolders) {
-			if(!charFolder.isDirectory()) {
+		for (final File charFolder : characterFolders) {
+			if (!charFolder.isDirectory()) {
 				continue;
 			}
 			final File[] characters = charFolder.listFiles();
-			for(final File characterFile : characters) {
-				if(!characterFile.isFile()) {
+			for (final File characterFile : characters) {
+				if (!characterFile.isFile()) {
 					continue;
 				}
 				try {
-					charSheet = JacksonInterface.loadCharacterSheet(characterFile, true);
+					charSheet = JacksonInterface.loadCharacterSheet(
+							characterFile, true);
 					adapter.add(charSheet);
-				}
-				catch(Throwable e) {
+				} catch (Throwable e) {
 					e.printStackTrace();
 				}
 			}
